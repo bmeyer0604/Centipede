62b3eed22b9ee67165eda049257c99a102cd195a
Brett Meyer <meyer.brett.da@gmail.com>
2019-10-15 16:32:58 -0700

    Tuesday afternoon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 const Game = require("../lib/game");
-const MovingObject = require("../lib/moving_object");
-
-const What = new MovingObject({pos: [0,0], vel: [0,1], radius: 4, color: "#ff0000"});
 
 document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.getElementById("centipede");
@@ -11,6 +8,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     canvas.width = game.DIM_X;
     canvas.height = game.DIM_Y;
-
-    What.draw(ctx);
 })
