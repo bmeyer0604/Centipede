684b3a9840d421902ddefb8df27718639f03e3d7
Brett Meyer <meyer.brett.da@gmail.com>
2019-10-16 14:07:24 -0700

    scorpion

--- src/util.js
+++ src/util.js
@@ -1,13 +1,22 @@
 const Util = {
     inherits(childClass, parentClass) {
-
     },
 
     dist(pos1, pos2) {
         return Math.sqrt(
             Math.pow(pos1[0] - pos2[0], 2) + Math.pow(pos1[1] - pos2[1], 2)
         );
-    }
+    },
+
+    // sprite(options) {
+    //     var that = {};
+    //     that.context = options.context;
+    //     that.width = options.width;
+    //     that.height = options.height;
+    //     that.image = options.image;
+    
+    //     return that;
+    // }
 }
 
 module.exports = Util;
