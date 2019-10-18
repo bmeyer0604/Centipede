4c126b67da8a82bbe75db3d2f4d1eecc574abc81
Brett Meyer <meyer.brett.da@gmail.com>
2019-10-17 22:20:00 -0700

    some pathfinding stuff

--- src/util.js
+++ src/util.js
@@ -6,17 +6,7 @@ const Util = {
         return Math.sqrt(
             Math.pow(pos1[0] - pos2[0], 2) + Math.pow(pos1[1] - pos2[1], 2)
         );
-    },
-
-    // sprite(options) {
-    //     var that = {};
-    //     that.context = options.context;
-    //     that.width = options.width;
-    //     that.height = options.height;
-    //     that.image = options.image;
-    
-    //     return that;
-    // }
+    }
 }
 
 module.exports = Util;
