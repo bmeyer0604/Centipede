4ddcb2677bf7df83fedd5b33b90df0707a28ff2f
Brett Meyer <meyer.brett.da@gmail.com>
2019-10-25 10:52:18 -0700

    heroku

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const app = express()
+const path = require('path')
+const fetch = require('node-fetch')
+const PORT = process.env.PORT || 8000; // process.env accesses heroku's environment variables
+
+
+
+app.use(express.static('public'))
+
+
+app.get('/', (request, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+app.listen(PORT, () => {
+  console.log(__dirname);
+  console.log(`listening on ${PORT}`)
+})
